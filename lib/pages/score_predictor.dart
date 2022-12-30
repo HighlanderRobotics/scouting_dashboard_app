dda5ba56575b23558f4b1e7a14cd0935d76f9f26
MangoSwirl <collin.camerono@gmail.com>
2022-12-30 14:58:23 -0800

    feat: use inputs in score predictor page

--- lib/pages/score_predictor.dart
+++ lib/pages/score_predictor.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:scouting_dashboard_app/analysis_functions/score_predictor_analysis.dart';
 import 'package:scouting_dashboard_app/reusable/navigation_drawer.dart';
 
@@ -12,6 +13,15 @@ class ScorePredictor extends StatefulWidget {
 }
 
 class _ScorePredictorState extends State<ScorePredictor> {
+  String blue1FieldValue = "";
+  String blue2FieldValue = "";
+  String blue3FieldValue = "";
+  String red1FieldValue = "";
+  String red2FieldValue = "";
+  String red3FieldValue = "";
+
+  ScorePredictorAnalysis? analysisFunction;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,72 +30,151 @@ class _ScorePredictorState extends State<ScorePredictor> {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text("Red Alliance"),
-                  SizedBox(height: 10),
+                children: [
+                  const Text("Red Alliance"),
+                  const SizedBox(height: 10),
                   TextField(
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       filled: true,
                       labelText: "Team 1",
                     ),
+                    onChanged: (value) {
+                      setState(() {
+                        analysisFunction = null;
+                        red1FieldValue = value;
+                      });
+                    },
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                    keyboardType: TextInputType.number,
                   ),
-                  SizedBox(height: 10),
+                  const SizedBox(height: 10),
                   TextField(
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       filled: true,
                       labelText: "Team 2",
                     ),
+                    onChanged: (value) {
+                      setState(() {
+                        analysisFunction = null;
+                        red2FieldValue = value;
+                      });
+                    },
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                    keyboardType: TextInputType.number,
                   ),
-                  SizedBox(height: 10),
+                  const SizedBox(height: 10),
                   TextField(
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       filled: true,
                       labelText: "Team 3",
                     ),
+                    onChanged: (value) {
+                      setState(() {
+                        analysisFunction = null;
+                        red3FieldValue = value;
+                      });
+                    },
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                    keyboardType: TextInputType.number,
                   ),
                 ],
               ),
               const SizedBox(height: 20),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text("Blue Alliance"),
-                  SizedBox(height: 10),
+                children: [
+                  const Text("Blue Alliance"),
+                  const SizedBox(height: 10),
                   TextField(
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       filled: true,
                       labelText: "Team 1",
                     ),
+                    onChanged: (value) {
+                      setState(() {
+                        analysisFunction = null;
+                        blue1FieldValue = value;
+                      });
+                    },
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                    keyboardType: TextInputType.number,
                   ),
-                  SizedBox(height: 10),
+                  const SizedBox(height: 10),
                   TextField(
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       filled: true,
                       labelText: "Team 2",
                     ),
+                    onChanged: (value) {
+                      setState(() {
+                        analysisFunction = null;
+                        blue2FieldValue = value;
+                      });
+                    },
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                    keyboardType: TextInputType.number,
                   ),
-                  SizedBox(height: 10),
+                  const SizedBox(height: 10),
                   TextField(
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       filled: true,
                       labelText: "Team 3",
                     ),
+                    onChanged: (value) {
+                      setState(() {
+                        analysisFunction = null;
+                        blue3FieldValue = value;
+                      });
+                    },
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                    keyboardType: TextInputType.number,
                   ),
                 ],
               ),
               const SizedBox(height: 40),
-              TextButton(
-                onPressed: () {},
+              ElevatedButton(
+                onPressed: int.tryParse(red1FieldValue) == null ||
+                        int.tryParse(red2FieldValue) == null ||
+                        int.tryParse(red3FieldValue) == null ||
+                        int.tryParse(blue1FieldValue) == null ||
+                        int.tryParse(blue2FieldValue) == null ||
+                        int.tryParse(blue3FieldValue) == null
+                    ? null
+                    : () {
+                        setState(() {
+                          analysisFunction = ScorePredictorAnalysis(
+                            blue1: int.parse(blue1FieldValue),
+                            blue2: int.parse(blue2FieldValue),
+                            blue3: int.parse(blue3FieldValue),
+                            red1: int.parse(red1FieldValue),
+                            red2: int.parse(red2FieldValue),
+                            red3: int.parse(red3FieldValue),
+                          );
+                        });
+                      },
                 child: const Text("Predict"),
               ),
               const SizedBox(height: 40),
-              ScorePrediction(
-                analysis: ScorePredictorAnalysis(
-                    blue1: 1, blue2: 2, blue3: 3, red1: 4, red2: 5, red3: 6),
-              )
+              if (analysisFunction != null)
+                ScorePrediction(
+                  analysis: analysisFunction!,
+                )
             ],
           ),
         ),
