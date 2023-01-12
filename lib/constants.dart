1f971c9c6556adaff9ff1078586388b04efba709
MangoSwirl <collin.camerono@gmail.com>
2023-01-11 23:24:26 -0800

    feat: add 2023 tournaments

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,7 +4,12 @@ import 'package:shared_preferences/shared_preferences.dart';
 RegExp validServerAuthority = RegExp(
     "^((((?!-))(xn--)?[a-zA-Z0-9][a-zA-Z0-9-_]{0,61}[a-zA-Z0-9]{0,1}\\.(xn--)?([a-zA-Z0-9\\-]{1,61}|[a-zA-Z0-9-]{1,30}\\.[a-zA-Z]{2,}))|(localhost))(:\\d+)?\$");
 
-List<Tournament> tournamentList = <Tournament>[];
+List<Tournament> tournamentList = <Tournament>[
+  Tournament("2023cafr", "Central Valley Regional 2023"),
+  Tournament("2023camb", "Monterey Bay Regional 2023"),
+  Tournament("2023cmptx", "Houston World 2023"),
+  Tournament("2023cc", "Chezy Champs 2023"),
+];
 
 Tournament? getTournamentByKey(String key) {
   try {
