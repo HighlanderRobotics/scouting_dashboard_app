0859a92df174aa052806b4f2791c21e84afc6935
MangoSwirl <collin.camerono@gmail.com>
2023-01-11 22:27:56 -0800

    feat: remove season-specific data

--- lib/metrics.dart
+++ lib/metrics.dart
@@ -26,42 +26,4 @@ class MetricCategoryData {
   List<CategoryMetric> metrics;
 }
 
-List<MetricCategoryData> metricCategories = [
-  MetricCategoryData("Cargo", [
-    CategoryMetric(
-      localizedName: "Accuracy",
-      abbreviatedLocalizedName: "Accuracy",
-      valueVizualizationBuilder: (val) => "${((val as num) * 100).round()}%",
-      path: "cargoAccuracy",
-    ),
-    CategoryMetric(
-      localizedName: "Average count",
-      abbreviatedLocalizedName: "Avg count",
-      valueVizualizationBuilder: (val) => (val as num).round().toString(),
-      path: "cargoCount",
-    ),
-  ]),
-  MetricCategoryData("Climber", [
-    CategoryMetric(
-      localizedName: "Max climb",
-      abbreviatedLocalizedName: "Maximum climb",
-      valueVizualizationBuilder: (val) =>
-          ClimbingChallenge.values[val as int].name,
-      path: "climberMax",
-    ),
-  ]),
-  MetricCategoryData("Defense", [
-    CategoryMetric(
-      localizedName: "Frequency",
-      abbreviatedLocalizedName: "Frequency",
-      valueVizualizationBuilder: (val) => "${val.round()}/5",
-      path: "defenseQuantity",
-    ),
-    CategoryMetric(
-      localizedName: "Success",
-      abbreviatedLocalizedName: "Success",
-      valueVizualizationBuilder: (val) => "${val.round()}/5",
-      path: "defenseQuality",
-    ),
-  ]),
-];
+List<MetricCategoryData> metricCategories = [];
