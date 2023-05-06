292239890127e1ea23d5ad6911b616f4f70ac0e4
MangoSwirl <collin.camerono@gmail.com>
2023-05-05 21:14:02 -0700

    Add fields for `SafeArea` sides

--- lib/reusable/page_body.dart
+++ lib/reusable/page_body.dart
@@ -5,15 +5,28 @@ class PageBody extends StatelessWidget {
     super.key,
     required this.child,
     this.padding = const EdgeInsets.fromLTRB(24, 16, 24, 16),
+    this.left = true,
+    this.right = true,
+    this.top = true,
+    this.bottom = true,
   });
   final Widget child;
   final EdgeInsets padding;
+
+  final bool left;
+  final bool right;
+  final bool top;
+  final bool bottom;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Theme.of(context).colorScheme.background,
       child: SafeArea(
+        left: left,
+        right: right,
+        top: top,
+        bottom: bottom,
         child: Padding(
           padding: padding,
           child: LayoutBuilder(builder: (context, constraints) {
