f4d6b1d9b6cf37bfd34ae354b38de6139d28476f
MangoSwirl <collin.camerono@gmail.com>
2023-08-08 21:37:16 -0700

    Add `ValueTile` widget

---
+++ lib/reusable/value_tile.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:scouting_dashboard_app/reusable/color_combination.dart';
+import 'package:scouting_dashboard_app/reusable/emphasized_container.dart';
+
+class ValueTile extends StatelessWidget {
+  const ValueTile({
+    super.key,
+    required this.value,
+    required this.label,
+    this.colorCombination = ColorCombination.plain,
+  });
+
+  final Widget value;
+  final Widget label;
+  final ColorCombination colorCombination;
+
+  @override
+  Widget build(BuildContext context) {
+    return EmphasizedContainer(
+      color: colorCombination.getBackgroundColor(context),
+      child: Column(children: [
+        DefaultTextStyle(
+          style: Theme.of(context)
+              .textTheme
+              .headlineSmall!
+              .copyWith(color: colorCombination.getForegroundColor(context)),
+          child: value,
+        ),
+        DefaultTextStyle(
+          style: Theme.of(context)
+              .textTheme
+              .labelLarge!
+              .copyWith(color: colorCombination.getForegroundColor(context)),
+          child: label,
+        ),
+      ]),
+    );
+  }
+}
