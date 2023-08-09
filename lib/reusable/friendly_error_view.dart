769582c7da5ecfe24e401ca8e7791caf7f7f291e
MangoSwirl <collin.camerono@gmail.com>
2023-08-08 21:37:47 -0700

    Add `FriendlyErrorView` widget

---
+++ lib/reusable/friendly_error_view.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class FriendlyErrorView extends StatelessWidget {
+  const FriendlyErrorView({
+    super.key,
+    this.errorMessage,
+    this.onRetry,
+  });
+
+  final String? errorMessage;
+  final dynamic Function()? onRetry;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            ":(",
+            style: Theme.of(context).textTheme.displayLarge,
+          ),
+          const SizedBox(height: 10),
+          Text(
+            "An ${errorMessage == null ? "unknown " : ""}error has occured.",
+            style: Theme.of(context).textTheme.headlineLarge,
+          ),
+          if (errorMessage != null) ...[
+            const SizedBox(height: 5),
+            Text(
+              errorMessage!,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+          ],
+          if (onRetry != null) ...[
+            const SizedBox(height: 24),
+            FilledButton(onPressed: onRetry, child: const Text("Retry")),
+          ]
+        ],
+      ),
+    );
+  }
+}
