f99983286f48f42f17320d7aba6f387ba3991b7f
MangoSwirl <collin.camerono@gmail.com>
2023-08-12 23:05:17 -0700

    Make warnings higher contrast on light mode

--- lib/color_schemes.g.dart
+++ lib/color_schemes.g.dart
@@ -86,6 +86,6 @@ extension ColorSchemeExtension on ColorScheme {
       : const Color(0xFFA2A7D0);
 
   Color get warningText => brightness == Brightness.light
-      ? const Color(0xFFE7EC00)
+      ? Color.fromARGB(255, 223, 235, 97)
       : const Color(0xFFE7EC00);
 }
