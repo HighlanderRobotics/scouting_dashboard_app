55b2541d1e3520271c632754f48b09c7a81b8a75
MangoSwirl <collin.camerono@gmail.com>
2024-01-24 18:37:17 -0800

    Implement publish button for new API

--- lib/pages/picklist/picklist_models.dart
+++ lib/pages/picklist/picklist_models.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:scouting_dashboard_app/constants.dart';
+import 'package:scouting_dashboard_app/reusable/lovat_api.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:uuid/uuid.dart';
 import 'package:http/http.dart' as http;
@@ -73,24 +74,7 @@ class ConfiguredPicklist {
   }
 
   Future<void> upload() async {
-    String authority = (await getServerAuthority())!;
-    final prefs = await SharedPreferences.getInstance();
-
-    final response =
-        await http.get(Uri.http(authority, '/API/manager/addPicklist', {
-      'uuid': id,
-      'name': title,
-      'team': prefs.getInt('team').toString(),
-      ...(weights.asMap().map(
-            (index, weight) =>
-                MapEntry<String, dynamic>(weight.path, weight.value.toString()),
-          )),
-      if (author != null) 'userName': author,
-    }));
-
-    if (response.statusCode != 200) {
-      throw "${response.statusCode} ${response.reasonPhrase}: ${response.body}";
-    }
+    await lovatAPI.sharePicklist(this);
   }
 
   String toJSON() => jsonEncode({
--- lib/reusable/lovat_api.dart
+++ lib/reusable/lovat_api.dart
@@ -439,6 +439,26 @@ class LovatAPI {
       'flags': json['flags'] as List<dynamic>? ?? [],
     };
   }
+
+  Future<void> sharePicklist(
+    ConfiguredPicklist picklist,
+  ) async {
+    // POST /v1/manager/picklists
+    final response = await post(
+      '/v1/manager/picklists',
+      body: {
+        'name': picklist.title,
+        ...Map.fromEntries(
+          picklist.weights.map((e) => MapEntry(e.path, e.value)),
+        ),
+      },
+    );
+
+    if (response?.statusCode != 200) {
+      debugPrint(response?.body ?? '');
+      throw Exception('Failed to share picklist');
+    }
+  }
 }
 
 class LovatAPIException implements Exception {
