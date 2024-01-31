6d2d265e02381b263cdb74f900facc1cd2e3c0f4
MangoSwirl <collin.camerono@gmail.com>
2024-01-31 12:52:04 -0800

    Start tournament match labeling

--- lib/pages/team_lookup/team_lookup_details.dart
+++ lib/pages/team_lookup/team_lookup_details.dart
@@ -380,144 +380,99 @@ class AnalysisOverview extends AnalysisVisualization {
                 ? const Center(
                     child: Text("No matches"),
                   )
-                : FutureBuilder(future: (() async {
-                    return await TournamentSchedule.fromServer(
-                      (await getServerAuthority())!,
-                      (await SharedPreferences.getInstance())
-                          .getString("tournament")!,
-                    );
-                  })(), builder: (context, scheduleSnapshot) {
-                    if (scheduleSnapshot.connectionState !=
-                        ConnectionState.done) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: LineChart(
-                        LineChartData(
-                          minY: 0,
-                          maxY: max,
-                          titlesData: FlTitlesData(
-                            bottomTitles: AxisTitles(
-                                axisNameWidget: const Text("Match"),
-                                sideTitles: SideTitles(
-                                  interval: 1,
-                                  showTitles: true,
-                                  reservedSize: 55,
-                                  getTitlesWidget: (value, meta) => RotatedBox(
-                                    quarterTurns: 1,
-                                    child: Text(
-                                      value.roundToDouble() == value &&
-                                              (snapshot.data['array']
-                                                      as List<dynamic>)
-                                                  .map((e) =>
-                                                      (e['match'] as String)
-                                                          .replaceAll(
-                                                              RegExp('_\\d+\$'),
-                                                              ""))
-                                                  .contains(scheduleSnapshot
-                                                      .data!
-                                                      .matches[
-                                                          value.toInt() - 1]
-                                                      .identity
-                                                      .toMediumKey())
-                                          ? scheduleSnapshot
-                                              .data!
-                                              .matches[value.toInt() - 1]
-                                              .identity
-                                              .getShortLocalizedDescription()
-                                          : "",
+                : Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: LineChart(
+                      LineChartData(
+                        minY: 0,
+                        maxY: max,
+                        lineTouchData: LineTouchData(
+                            touchTooltipData: LineTouchTooltipData(
+                          getTooltipItems: (touchedSpots) {
+                            return touchedSpots
+                                .map((touchedSpot) => LineTooltipItem(
+                                      analysisFunction.metric
+                                          .valueVizualizationBuilder(
+                                        touchedSpot.y,
+                                      ),
+                                      Theme.of(context).textTheme.labelMedium!,
+                                    ))
+                                .toList();
+                          },
+                        )),
+                        titlesData: FlTitlesData(
+                          bottomTitles: AxisTitles(),
+                          topTitles: AxisTitles(),
+                          leftTitles: AxisTitles(
+                            axisNameWidget: Text(analysisFunction
+                                .metric.abbreviatedLocalizedName),
+                            sideTitles: SideTitles(
+                                showTitles: true,
+                                getTitlesWidget: (value, meta) =>
+                                    SideTitleWidget(
+                                      axisSide: meta.axisSide,
+                                      child: Text(
+                                        analysisFunction.metric == null
+                                            ? "--"
+                                            : analysisFunction.metric
+                                                .valueVizualizationBuilder(
+                                                    value),
+                                      ),
                                     ),
-                                  ),
-                                )),
-                            topTitles: AxisTitles(),
-                            leftTitles: AxisTitles(
-                              axisNameWidget: Text(analysisFunction
-                                  .metric.abbreviatedLocalizedName),
-                              sideTitles: SideTitles(
-                                  showTitles: true,
-                                  getTitlesWidget: (value, meta) =>
-                                      SideTitleWidget(
-                                        axisSide: meta.axisSide,
-                                        child: Text(
-                                          analysisFunction.metric == null
-                                              ? "--"
-                                              : analysisFunction.metric
-                                                  .valueVizualizationBuilder(
-                                                      value),
-                                        ),
-                                      ),
-                                  reservedSize: 50),
-                            ),
-                            rightTitles: AxisTitles(),
+                                reservedSize: 50),
                           ),
-                          borderData: FlBorderData(
-                            border: Border.all(
-                              color: Theme.of(context).colorScheme.outline,
-                            ),
-                          ),
-                          gridData: FlGridData(
-                            getDrawingHorizontalLine: (value) {
-                              return FlLine(
-                                color: Theme.of(context).colorScheme.outline,
-                                strokeWidth: 1,
-                              );
-                            },
-                            getDrawingVerticalLine: (value) {
-                              return FlLine(
-                                color: Theme.of(context).colorScheme.outline,
-                                strokeWidth: 1,
-                              );
-                            },
-                          ),
-                          lineBarsData: [
-                            LineChartBarData(
-                              spots: (() {
-                                final TournamentSchedule schedule =
-                                    scheduleSnapshot.data!;
-                                final List array = snapshot.data['array'];
-
-                                List<FlSpot> spots = [];
-
-                                for (var i = 0; i < array.length; i++) {
-                                  if (array[i]["value"] != null) {
-                                    spots.add(
-                                      FlSpot(
-                                        schedule.matches
-                                            .firstWhere(
-                                              (match) =>
-                                                  match.identity
-                                                      .toMediumKey() ==
-                                                  (array[i]["match"] as String)
-                                                      .replaceAll(
-                                                          RegExp('_\\d+\$'),
-                                                          ""),
-                                            )
-                                            .ordinalNumber
-                                            .toDouble(),
-                                        array[i]["value"].toDouble(),
-                                      ),
-                                    );
-                                  }
-                                }
-
-                                spots.sort((a, b) => a.x.compareTo(b.x));
-
-                                return spots;
-                              })(),
-                              isCurved: true,
-                              curveSmoothness: 0.4,
-                              preventCurveOverShooting: true,
-                              color: Theme.of(context).colorScheme.primary,
-                            ),
-                          ],
+                          rightTitles: AxisTitles(),
                         ),
+                        borderData: FlBorderData(
+                          border: Border.all(
+                            color: Theme.of(context).colorScheme.outline,
+                          ),
+                        ),
+                        gridData: FlGridData(
+                          getDrawingHorizontalLine: (value) {
+                            return FlLine(
+                              color: Theme.of(context).colorScheme.outline,
+                              strokeWidth: 1,
+                            );
+                          },
+                          getDrawingVerticalLine: (value) {
+                            return FlLine(
+                              color: Theme.of(context).colorScheme.outline,
+                              strokeWidth: 1,
+                            );
+                          },
+                        ),
+                        lineBarsData: [
+                          LineChartBarData(
+                            spots: (() {
+                              final List array = snapshot.data['array'];
+
+                              List<FlSpot> spots = [];
+
+                              for (var i = 0; i < array.length; i++) {
+                                if (array[i]["value"] != null) {
+                                  spots.add(
+                                    FlSpot(
+                                      i.toDouble(),
+                                      array[i]["value"].toDouble(),
+                                    ),
+                                  );
+                                }
+                              }
+
+                              spots.sort((a, b) => a.x.compareTo(b.x));
+
+                              return spots;
+                            })(),
+                            isCurved: true,
+                            curveSmoothness: 0.4,
+                            preventCurveOverShooting: true,
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                        ],
                       ),
-                    );
-                  }),
+                    ),
+                  ),
           ),
         ),
         const SizedBox(height: 11),
@@ -666,19 +621,8 @@ class _ScoringBreakdownState extends State<ScoringBreakdown> {
                                                             constraints
                                                                 .maxHeight) /
                                                         2,
-                                                    title: "${(data![e]!
-                                                                    .toDouble() *
-                                                                100)
-                                                            .round()}%\n${scoringMethods.any(
-                                                                (f) =>
-                                                                    f.path == e)
-                                                            ? scoringMethods
-                                                                .firstWhere(
-                                                                    (f) =>
-                                                                        f.path ==
-                                                                        e)
-                                                                .localizedName
-                                                            : e}",
+                                                    title:
+                                                        "${(data![e]!.toDouble() * 100).round()}%\n${scoringMethods.any((f) => f.path == e) ? scoringMethods.firstWhere((f) => f.path == e).localizedName : e}",
                                                     value: data![e]!.toDouble(),
                                                     color: Theme.of(context)
                                                         .colorScheme
