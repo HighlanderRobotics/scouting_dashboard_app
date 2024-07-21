1ce671fe31f27c564d9bcc5b7db26c058ad3c597
MangoSwirl <collin.camerono@gmail.com>
2024-07-21 00:20:00 -0700

    Refactor out `lovatAPI.createScoutScheduleShift()`

--- lib/reusable/lovat_api/lovat_api.dart
+++ lib/reusable/lovat_api/lovat_api.dart
@@ -133,39 +133,6 @@ class LovatAPI {
 
   // MARK: Endpoints
 
-  Future<void> createScoutScheduleShift(ScoutingShift shift) async {
-    final tournament = await Tournament.getCurrent();
-
-    if (tournament == null) {
-      throw const LovatAPIException('No tournament selected');
-    }
-
-    final response = await post(
-      '/v1/manager/tournament/${tournament.key}/scoutershifts',
-      body: {
-        'startMatchOrdinalNumber': shift.start,
-        'endMatchOrdinalNumber': shift.end,
-        'team1': shift.team1.map((e) => e.id).toList(),
-        'team2': shift.team2.map((e) => e.id).toList(),
-        'team3': shift.team3.map((e) => e.id).toList(),
-        'team4': shift.team4.map((e) => e.id).toList(),
-        'team5': shift.team5.map((e) => e.id).toList(),
-        'team6': shift.team6.map((e) => e.id).toList(),
-      },
-    );
-
-    if (response?.statusCode != 200) {
-      try {
-        throw LovatAPIException(jsonDecode(response!.body)['displayError']);
-      } on LovatAPIException {
-        rethrow;
-      } catch (_) {
-        debugPrint(response?.body ?? '');
-        throw Exception('Failed to create scouter schedule shift');
-      }
-    }
-  }
-
   Future<Map<String, dynamic>> getMetricDetails(
       int teamNumber, String metricPath) async {
     final response =
