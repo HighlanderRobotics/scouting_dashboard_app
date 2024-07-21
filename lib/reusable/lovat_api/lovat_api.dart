aad1b46fa0487a7b928bbcc08d3a31691464368b
MangoSwirl <collin.camerono@gmail.com>
2024-07-21 00:53:11 -0700

    Refactor out `lovatAPI.setNotOnTeam()`

--- lib/reusable/lovat_api/lovat_api.dart
+++ lib/reusable/lovat_api/lovat_api.dart
@@ -153,21 +153,6 @@ class LovatAPI {
     }
   }
 
-  Future<void> setNotOnTeam() async {
-    final response = await lovatAPI.post('/v1/manager/noteam');
-
-    if (response?.statusCode != 200) {
-      debugPrint(response?.body ?? '');
-      try {
-        throw LovatAPIException(jsonDecode(response!.body)['displayError']);
-      } on LovatAPIException {
-        rethrow;
-      } catch (_) {
-        throw Exception('Failed to set not on team');
-      }
-    }
-  }
-
   Future<List<ScouterOverview>> getScouterOverviews() async {
     final tournament = await Tournament.getCurrent();
 
