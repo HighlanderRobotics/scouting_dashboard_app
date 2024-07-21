20f34af5798536a3df76f0188ecfeaaf95c4b23a
MangoSwirl <collin.camerono@gmail.com>
2024-07-21 00:12:44 -0700

    Refactor out `lovatAPI.updateScouterScheduleShift()`

--- lib/reusable/lovat_api/lovat_api.dart
+++ lib/reusable/lovat_api/lovat_api.dart
@@ -133,37 +133,6 @@ class LovatAPI {
 
   // MARK: Endpoints
 
-  Future<void> updateScouterScheduleShift(
-    ServerScoutingShift shift,
-  ) async {
-    debugPrint(shift.team1.toString());
-
-    final response = await post(
-      '/v1/manager/scoutershifts/${shift.id}',
-      body: {
-        'startMatchOrdinalNumber': shift.start,
-        'endMatchOrdinalNumber': shift.end,
-        'team1': shift.team1.map((e) => e.id).toList(),
-        'team2': shift.team2.map((e) => e.id).toList(),
-        'team3': shift.team3.map((e) => e.id).toList(),
-        'team4': shift.team4.map((e) => e.id).toList(),
-        'team5': shift.team5.map((e) => e.id).toList(),
-        'team6': shift.team6.map((e) => e.id).toList(),
-      },
-    );
-
-    if (response?.statusCode != 200) {
-      try {
-        throw LovatAPIException(jsonDecode(response!.body)['displayError']);
-      } on LovatAPIException {
-        rethrow;
-      } catch (_) {
-        debugPrint(response?.body ?? '');
-        throw Exception('Failed to update scouter schedule shift');
-      }
-    }
-  }
-
   Future<List<Scout>> getScouts() async {
     final response = await get('/v1/manager/scoutershift/scouters');
 
---
+++ lib/reusable/lovat_api/scouter_schedule/update_scouter_schedule_shift.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:frc_8033_scouting_shared/frc_8033_scouting_shared.dart';
+import 'package:scouting_dashboard_app/reusable/lovat_api/lovat_api.dart';
+
+extension UpdateScouterScheduleShift on LovatAPI {
+  Future<void> updateScouterScheduleShift(
+    ServerScoutingShift shift,
+  ) async {
+    debugPrint(shift.team1.toString());
+
+    final response = await post(
+      '/v1/manager/scoutershifts/${shift.id}',
+      body: {
+        'startMatchOrdinalNumber': shift.start,
+        'endMatchOrdinalNumber': shift.end,
+        'team1': shift.team1.map((e) => e.id).toList(),
+        'team2': shift.team2.map((e) => e.id).toList(),
+        'team3': shift.team3.map((e) => e.id).toList(),
+        'team4': shift.team4.map((e) => e.id).toList(),
+        'team5': shift.team5.map((e) => e.id).toList(),
+        'team6': shift.team6.map((e) => e.id).toList(),
+      },
+    );
+
+    if (response?.statusCode != 200) {
+      try {
+        throw LovatAPIException(jsonDecode(response!.body)['displayError']);
+      } on LovatAPIException {
+        rethrow;
+      } catch (_) {
+        debugPrint(response?.body ?? '');
+        throw Exception('Failed to update scouter schedule shift');
+      }
+    }
+  }
+}
